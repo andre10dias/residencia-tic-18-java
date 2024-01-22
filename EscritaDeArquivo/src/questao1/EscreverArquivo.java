46340187c414e03bfc7eb7272dab186d1613f3c2
andre10dias <andre10dias@ymail.com>
2024-01-22 12:04:00 -0300

    Escrita de arquivo json

--- EscritaDeArquivo/src/questao1/EscreverArquivo.java
+++ EscritaDeArquivo/src/questao1/EscreverArquivo.java
@@ -1,9 +1,13 @@
 package questao1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import org.json.JSONObject;
 
 import model.Estudante;
 
@@ -67,11 +71,43 @@ public class EscreverArquivo {
 	
 	private static void gravarArquivo(List<Estudante> lista) {
 		String nomeArquivo = "estudantes.json";
-		try {
+		JSONObject json = new JSONObject();
+
+		try (Scanner scanner = new Scanner(System.in);
+	             BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
+			for (Estudante e : lista) {
+				json.put("nome", e.getNome());
+				json.put("cpf", e.getCpf());
+				json.put("cra", e.getCra());
+				json.put("anoDeAdmissao", e.getAnoDeAdmissão());
+				
+				writer.write(json.toString());
+                writer.newLine();
+			}
+			
+			System.out.println(json);
 			System.out.println("O arquivo " + nomeArquivo + ", foi salvo com sucesso.");
 		} catch (Exception e) {
 			System.out.println("\nFalha ao gravar o arquivo: " + e.getMessage());
 		}
 	}
+	
+//	private static List<Estudante> mock() {
+//		List<Estudante> lista = new ArrayList<>();
+//		
+//		Estudante estudante = new Estudante("Carlos", "123456879", 2.3f, 2023);
+//		lista.add(estudante);
+//		
+//		Estudante estudante1 = new Estudante("Maria", "089713152", 3f, 2023);
+//		lista.add(estudante1);
+//		
+//		Estudante estudante2 = new Estudante("João", "852123963", 5f, 2022);
+//		lista.add(estudante2);
+//		
+//		Estudante estudante3 = new Estudante("Franciele", "7892563011", 3.1f, 2020);
+//		lista.add(estudante3);
+//		
+//		return lista;
+//	}
 
 }
