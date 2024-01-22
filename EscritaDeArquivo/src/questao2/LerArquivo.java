fdc1514735a73cf1d1c20e26acdc11ef716ec16e
andre10dias <andre10dias@ymail.com>
2024-01-22 12:18:42 -0300

    Leitura de arquivo json

--- EscritaDeArquivo/src/questao2/LerArquivo.java
+++ EscritaDeArquivo/src/questao2/LerArquivo.java
@@ -4,10 +4,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import model.Estudante;
 
 public class LerArquivo {
 	public static void main(String[] args) {
-        String nomeArquivo = "entrada.txt";
+        String nomeArquivo = "estudantes.json";
         File arquivo = new File(nomeArquivo);
 
         if (!arquivo.exists()) {
@@ -21,10 +27,14 @@ public class LerArquivo {
          * */
         try (FileReader fileReader = new FileReader(arquivo);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+        	List<Estudante> lista = new ArrayList<>();
 
             String linha;
             System.out.println("Lendo arquivo " + nomeArquivo + "...\n");
             while ((linha = bufferedReader.readLine()) != null) {
+//            	JSONObject json = new JSONObject();
+//            	json.getJSONArray(linha);
+//                System.out.println(json);
                 System.out.println(linha);
             }
 
